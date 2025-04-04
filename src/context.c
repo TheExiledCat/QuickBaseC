ab077f8e7b8382a693daf359f33474ebf289d7c2
TheExiledCat <Kazekage2002@hotmail.com>
2025-04-04 15:38:42 +0200

    added command line parser

--- src/context.c
+++ src/context.c
@@ -1,18 +1,28 @@
 #include "context.h"
+#include "model.h"
+#include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #define DATABASE_DIR "data/"
 #define DATABASE_NAME "qb_data.sqlite"
 #define DATABASE_PATH DATABASE_DIR DATABASE_NAME
+
+bool GetTableQuery(Entity *entity, sqlite3_stmt **stmt);
+char *GetFieldQuery(EntityField *field);
 sqlite3 *conn;
+
+char *EntityTypeToSQLITE(EntityField *field);
 bool EnsureDatabaseCreation(Schema *schema) {
 
   struct stat st = {0};
   if (stat(DATABASE_DIR, &st) == -1) {
+    printf("Data dir not found, creating\n");
     mkdir(DATABASE_DIR, 0700);
   }
-  if (access(DATABASE_PATH, F_OK)) {
+  if (!access(DATABASE_PATH, F_OK)) {
+    printf("database already exists\n");
     return true;
   }
   sqlite3_open(DATABASE_PATH, &conn);
@@ -30,4 +40,51 @@ bool Init(Schema *schema) {
   }
   return true;
 }
-bool CreateTables(Schema *schema) { return true; }
+bool CreateTables(Schema *schema) {
+  printf("Creating tables\n");
+  for (int i = 0; i < schema->entityCount; i++) {
+
+    sqlite3_stmt *query;
+    Entity *entity = schema->entities[i];
+    printf("creating table %s\n", entity->name);
+    GetTableQuery(entity, &query);
+    int result = sqlite3_step(query);
+    if (result != SQLITE_OK && result != SQLITE_DONE) {
+      printf("%s", sqlite3_errmsg(conn));
+    }
+  }
+  return true;
+}
+bool GetTableQuery(Entity *entity, sqlite3_stmt **stmt) {
+  char *query = malloc(0xFFF);
+  sprintf(query, "CREATE TABLE IF NOT EXISTS %s (", entity->name);
+  for (int i = 0; i < entity->fieldCount; i++) {
+    EntityField *field = entity->fields[i];
+    sprintf(query, "%s %s %s%s", query, field->name, EntityTypeToSQLITE(field),
+            i == entity->fieldCount - 1 ? "" : ",");
+  }
+  sprintf(query, "%s)", query);
+  printf("query: %s\n", query);
+  sqlite3_prepare(conn, query, -1, stmt, NULL);
+  free(query);
+  return true;
+}
+char *GetFieldQuery(EntityField *field) { return ""; }
+char *EntityTypeToSQLITE(EntityField *field) {
+  switch (field->type) {
+
+  case FIELD_TEXT:
+  case FIELD_DATE:
+    return "TEXT";
+
+  case FIELD_NUMBER: {
+    NumberField *numField = (NumberField *)field;
+    return numField->isFloat ? "REAL" : "INTEGER";
+  }
+
+  case FIELD_RELATION:
+    return "INTEGER";
+  case FIELD_BOOL:
+    return "INTEGER";
+  }
+}
--- src/main.c
+++ src/main.c
@@ -2,13 +2,52 @@
 #include "converter.h"
 #include "mongoose.h"
 #include "schema.h"
+#include <argp.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char *argv[]) {
-  Schema *schema = DefaultSchema();
-  const char *json;
-  ConvertSchema(schema, &json);
-  Init(schema);
+
+#include "argparse.h"
+typedef struct command {
+  char *name;
+  int (*action)(int, const char **);
+} command;
+int Serve(int argc, const char **argv);
+int Tui(int argc, const char **argv);
+static const char *usages[] = {"command [options]", "serve -p <PORT>", "tui",
+                               NULL};
+static command commands[] = {{"serve", Serve}, {"tui", Tui}};
+int main(int argc, const char *argv[]) {
+  struct argparse argp;
+  struct argparse_option basicOptions[] = {OPT_HELP(), OPT_END()};
+  argparse_init(&argp, basicOptions, usages, ARGPARSE_STOP_AT_NON_OPTION);
+  argc = argparse_parse(&argp, argc, argv);
+  if (argc < 1) {
+    argparse_usage(&argp);
+    return -1;
+  }
+  command *cmd = NULL;
+  for (int i = 0; i < sizeof(commands) / sizeof(command); i++) {
+
+    if (!strcmp(commands[i].name, argv[0])) {
+      cmd = &commands[i];
+    }
+  }
+  if (cmd) {
+    return cmd->action(argc, argv);
+  } else {
+    return -1;
+  }
+  // Schema *schema = DefaultSchema();
+  // const char *json;
+  // ConvertSchema(schema, &json);
+  // Init(schema);
+
+  return 0;
+}
+int Tui(int argc, const char *argv[]) { return 0; }
+int Serve(int argc, const char *argv[]) {
+  printf("Hello World");
+  return 0;
 }
